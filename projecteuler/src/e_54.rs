61e075755b0843664128a99cad8b754f449f5a98
Daniel Lee <daniel@dotmodus.com>
2020-05-30 14:29:38 +0100

    :roll_eyes: This problem has become tedious

--- projecteuler/src/e_54.rs
+++ projecteuler/src/e_54.rs
@@ -31,6 +31,8 @@ How many hands does Player 1 win?
 use std::fs;
 use std::time::Instant;
 use std::collections::HashMap;
+use std::cmp::max;
+
 
 fn straight(hand: &str) -> bool {
     let mut options: Vec<&str> = vec![];
@@ -152,8 +154,52 @@ fn straight_flush(hand: &str) -> bool {
     return straight(hand) && flush(hand)
 }
 
-fn tests() {
+fn highest_set(hand: &str) -> &str {
+    let mut cards: Vec<&str> = vec![];
+    for card in hand.split(" ") {
+        let f: &str = &card[0..1];
+        if cards.contains(&f) {
+            return f
+        }
+        cards.push(f)
+    }
+    &hand[13..14]
+}
+
+fn highest_card(hand: &str) -> &str {
+    let mut cards: Vec<&str> = vec![];
+    for card in hand.split(" ") {
+        cards.push(&card[0..1])
+    }
+    if cards.contains(&"A") {
+        return "A"
+    } else if cards.contains(&"K") {
+        return "K"
+    } else if cards.contains(&"Q") {
+        return "Q"
+    } else if cards.contains(&"J") {
+        return "J"
+    } else if cards.contains(&"T") {
+        return "T"
+    }
+    let mut current_card = "2";
+    for card in cards {
+        current_card = max(card, current_card)
+    }
+    current_card
+}
+
+fn _tests() {
     for _ in 0..1 {
+        assert_eq!(highest_card("2D 3D 4D 5D 6D"), "6");
+        assert_eq!(highest_card("2D 2D 2D 2D 2D"), "2");
+        assert_eq!(highest_card("2D 3D 9D AD 2D"), "A");
+        assert_eq!(highest_card("2D KD 9D AD 2D"), "A");
+        assert_eq!(highest_card("2D QD 9D TD 2D"), "Q");
+        assert_eq!(highest_card("2D 6D 9D TD 2D"), "T");
+        assert_eq!(highest_set("5D KC 3H 5S AC"), "5");
+        assert_eq!(highest_set("4S 7H QS 4C 2H"), "4");
+        assert_eq!(highest_set("4S 7H QS 2C 2H"), "2");
         assert!(straight("AS 2S 3C 4C 5S"));
         assert!(straight("6S 2S 3C 4C 5S"));
         assert!(straight("TS JS KC QC AS"));
@@ -188,15 +234,107 @@ pub fn main() {
     let now = Instant::now();
     let data = fs::read_to_string("/Users/daniellee/Development/projecteuler/projecteuler/src/poker.txt").expect("Unable to read file");
     let hands: Vec<&str> = data.split("\n").collect();
-    tests();
+    _tests();
     let mut wins = 0;
     for row in hands {
         let hand1 = &row[0..14];
         let hand2 = &row[15..29];
-        if royal_flush(hand1) && ! royal_flush(hand2){
+        if royal_flush(hand1) && !royal_flush(hand2){
             wins += 1;
+            println!("{}", hand1);
         }
-        break;
+        else if straight_flush(hand1) && !royal_flush(hand2) && !straight_flush(hand2) {
+            wins += 1;
+            println!("{}", hand1);
+        }
+        let h1_sets = sets(hand1);
+        let h2_sets = sets(hand2);
+        // 4 of a kind
+        if h1_sets[4] && !h2_sets[4] && !straight_flush(hand2) && !royal_flush(hand2) {
+            wins += 1;
+            println!("{}", hand1);
+        }
+        //Full house
+        else if h1_sets[2] && !h2_sets[2] && !h2_sets[4] && !straight_flush(hand2) && !royal_flush(hand2) {
+            wins += 1;
+            println!("{}", hand1);
+        }
+        // Flush
+        else if flush(hand1) && !flush(hand2) && !h2_sets[2] && !h2_sets[4] && !straight_flush(hand2) && !royal_flush(hand2) {
+            wins += 1;
+            println!("{}", hand1);
+        }
+        // Straight
+        else if straight(hand1) && !straight(hand2) && !flush(hand2) && !h2_sets[2] && !h2_sets[4] && !straight_flush(hand2) && !royal_flush(hand2) {
+            wins += 1;
+            println!("{}", hand1);
+        }
+        // 3 of a kind
+        else if h1_sets[3] {
+            if !h2_sets[3]  && !straight(hand2) && !flush(hand2) && !h2_sets[2] && !h2_sets[4] && !straight_flush(hand2) && !royal_flush(hand2) {
+                wins += 1;
+//                println!("{}", hand1);
+            } else if h2_sets[3] {
+                println!("{}, {}", hand2, hand1);
+            }
+        }
+        // 2 pairs
+        else if h1_sets[1] {
+            if !h2_sets[1] && !straight(hand2) && !flush(hand2) && !h2_sets[3] && !h2_sets[2] && !h2_sets[4] && !straight_flush(hand2) && !royal_flush(hand2) {
+               wins += 1;
+            } else if h2_sets[1] {
+            println!("{}, {}", hand1, hand2)
+            }
+        }
+        else if h1_sets[0] {
+            if !h2_sets[0] && !straight(hand2) && !flush(hand2) && !h2_sets[1] && !h2_sets[3] && !h2_sets[2] && !h2_sets[4] && !straight_flush(hand2) && !royal_flush(hand2) {
+               wins += 1;
+            } else if h2_sets[0] {
+                let hs1 = highest_set(hand1);
+                let hs2 = highest_set(hand2);
+                if hs1 == "A" {
+                    wins += 1
+                }
+                else if hs1 == "K" && hs2 != "A" {
+                    wins += 1
+                }
+                else if hs1 == "Q" && !vec!["A", "K"].contains(&hs2){
+                    wins += 1
+                }
+                else if hs1 == "J" && !vec!["A", "K", "Q"].contains(&hs2){
+                    wins +=1
+                }
+                else if hs1 == "T" && !vec!["A", "K", "Q", "T"].contains(&hs2) {
+                    wins += 1
+                }
+                else if hs1 > hs2 {
+                    wins += 1
+                }
+            }
+        }
+        // Highest card
+        else if !h2_sets[0] && !straight(hand2) && !flush(hand2) && !h2_sets[1] && !h2_sets[3] && !h2_sets[2] && !h2_sets[4] && !straight_flush(hand2) && !royal_flush(hand2) {
+            let hc1 = highest_card(hand1);
+            let hc2 = highest_card(hand2);
+            if hc1 == "A" {
+                    wins += 1
+                }
+                else if hc1 == "K" && hc2 != "A" {
+                    wins += 1
+                }
+                else if hc1 == "Q" && !vec!["A", "K"].contains(&hc2){
+                    wins += 1
+                }
+                else if hc1 == "J" && !vec!["A", "K", "Q"].contains(&hc2){
+                    wins +=1
+                }
+                else if hc1 == "T" && !vec!["A", "K", "Q", "T"].contains(&hc2) {
+                    wins += 1
+                }
+                else if hc1 > hc2 {
+                    wins += 1
+                }
+        }
     }
     println!("Total wins for Player 1: {}", wins);
     println!("Script took {} milliseconds to run", now.elapsed().as_millis());
